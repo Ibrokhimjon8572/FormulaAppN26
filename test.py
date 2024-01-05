043b415059ed171e24d4ba1e1015cc0e6737f7b3
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2024-01-05 14:19:03 +0500

    lesson utils

---
+++ test.py
@@ -0,0 +1,45 @@
+# def somefuncg():#generator
+#     count = 1
+#     for i in range(1, 10):
+#         yield count
+#         count *= i
+
+# a = somefuncg()
+
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+
+# for i in somefuncg():
+#     print(i)
+# def somefuncn():#normal function
+#     return 1
+    # return 2
+
+# print(somefuncn())
+# a = somefuncg()
+
+# print(a)
+
+# for i in somefuncg():
+#     print(i)
+
+# for j in somefunc():
+#     print(j)
+# print(somefunc())
+# print(somefunc())
+# print(somefunc())
+
+# for j in a:
+#     print(j)
+
+# print(a)
+# 1
+# 3
+# 9
+# 27
+# 81
+# .
+# .
+# .
